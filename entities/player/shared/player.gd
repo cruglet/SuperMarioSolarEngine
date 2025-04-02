ba364ad7f80b9e0c31de7a6a1d173866943a04c8
Cruglet <cruglet@gmail.com>
2025-04-02 14:36:33 -0400

    Initial branch commit
    This is mainly just for experimentation purposes.
    Using an AnimationTree could provide some benefits like a graphical version of the current state machine and even easier modification of state connections.

--- entities/player/shared/player.gd
+++ entities/player/shared/player.gd
@@ -4,16 +4,16 @@ extends CharacterBody2D
 
 @export var doll: AnimatedSprite2D
 
+@export_group("Managers")
 @export var state_manager: StateManager
 @export var movement: PMovement
 @export var input: InputManager
 
+@export_group("Collision")
 @export var push_rays: Node2D
-
 @export var hitbox: CollisionShape2D
 @export var dive_hitbox: CollisionShape2D
 @export var small_hitbox: CollisionShape2D
-
 @export var water_check: Area2D
 @export var crouchlock: CrouchlockDetection
 @export var spin_hitbox: Area2D
